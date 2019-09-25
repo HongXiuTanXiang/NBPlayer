7ea94edb0ca5f3dac35f82e98f337846581fbf41
“xsqk” <464429107@qq.com>
2019-09-25 14:41:58 +0800

    跑的通

--- NBPlayer/app/videoPlay/controller/VideoHorizontallyVideoVC.h
+++ NBPlayer/app/videoPlay/controller/VideoHorizontallyVideoVC.h
@@ -9,6 +9,7 @@
 #import <UIKit/UIKit.h>
 #import <IJKMediaFramework/IJKMediaFramework.h>
 #import "VideoHorizontallyViewModel.h"
+#import "MediaControlView.h"
 NS_ASSUME_NONNULL_BEGIN
 
 @interface VideoHorizontallyVideoVC : UIViewController
